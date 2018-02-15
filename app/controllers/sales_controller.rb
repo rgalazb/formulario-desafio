6304b0adb1042280393e69e7f5cf464a1281fc84
rgalazb <galaz.becerra@gmail.com>
2018-02-15 10:44:40 -0300

    commit

---
+++ app/controllers/sales_controller.rb
@@ -0,0 +1,32 @@
+class SalesController < ApplicationController
+  def new
+    @sale = Sale.new
+  end
+
+  def done
+    @sale = Sale.last
+  end
+
+  def create
+
+    cod = params['sale']['cod'].to_i
+    detail = params['sale']['detail']
+    category = params['sale']['category'].to_i
+    discount = params['sale']['discount'].to_i
+    value = params['sale']['value'].to_i
+    total = (100-discount) * value
+    if params.has_key?(:tax)
+      tax = 19
+    else
+      tax = 0
+    end
+
+    s = Sale.new(cod: cod, detail: detail, category: category, discount: discount, value: value, tax: tax, total: total)
+    
+    if s.save
+      redirect_to sales_done_path, notice: 'el producto se ingreso correctamente'
+    else
+      redirect_to sales_done_path, notice: 'se ingresó mal'
+    end
+  end
+end
---
+++ app/models/sale.rb
@@ -0,0 +1,11 @@
+class Sale < ApplicationRecord
+  validates :cod, uniqueness: true
+  validates :detail, presence: true
+  validates :category, inclusion: { in: [1,2,3,4] ,
+    message: "%{value} no es una categoría valida" }
+
+  validates :value, :discount, numericality: {greater_than_or_equal_to: 0, only_integer: true}
+
+  validates :discount, numericality: {less_than_or_equal_to: 40}
+
+end
