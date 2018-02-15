8411da6bc59a58aa6438b915c0dfa7e01f9a0899
rgalazb <galaz.becerra@gmail.com>
2018-02-15 11:19:39 -0300

    impuesto

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -17,12 +17,13 @@ class SalesController < ApplicationController
     total = (100-discount) * value
     if params.has_key?(:tax)
       tax = 19
+      total *= 1.19
     else
       tax = 0
     end
 
     s = Sale.new(cod: cod, detail: detail, category: category, discount: discount, value: value, tax: tax, total: total)
-    
+
     if s.save
       redirect_to sales_done_path, notice: 'el producto se ingreso correctamente'
     else
--- app/models/sale.rb
+++ app/models/sale.rb
@@ -4,8 +4,13 @@ class Sale < ApplicationRecord
   validates :category, inclusion: { in: [1,2,3,4] ,
     message: "%{value} no es una categoría valida" }
 
-  validates :value, :discount, numericality: {greater_than_or_equal_to: 0, only_integer: true}
+  validates :value, :discount, numericality: {
+    greater_than_or_equal_to: 0,
+    only_integer: true
+  }
 
-  validates :discount, numericality: {less_than_or_equal_to: 40}
+  validates :discount, numericality: {
+    less_than_or_equal_to: 40
+  }
 
 end
